9963018a6eba2535413a6cd1a29c8063d34c296d
CM Student <cmstudent@uhwo-acm-15.local>
2018-04-02 10:34:32 -1000

    Finished final hackwich. This is also the start of Final Project. Code for maps and centering location, as well as adding a restaurant.

--- HackwihcEight/ViewController.swift
+++ HackwihcEight/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet var mapView: MKMapView!
+    let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.05)
+    let regionRadius: CLLocationDistance = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        centerMapLocation(location: initialLocation)
+        
+        //create a restaurant object and set all the arguments to each of the three parameters
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate:
+            CLLocationCoordinate2D(latitude: 21.346478, longitude: -158.080098))
+        mapView.addAnnotation(restaurantOne)
+    
     }
 
+    func centerMapLocation (location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+            mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
